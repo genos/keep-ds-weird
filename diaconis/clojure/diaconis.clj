2a1b248330c35d4ec450d37d9bc1bf2390b9a66c
genos <graham@quantifind.com>
2017-12-18 14:47:44 -0500

    ps, not vecs; output formatting

--- diaconis/clojure/diaconis.clj
+++ diaconis/clojure/diaconis.clj
@@ -5,15 +5,15 @@
   [k 5
    n 500
    zero (m/zero-matrix k k)
-   vecs (for [_ (range n)] (shuffle (range k)))
-   a (->> vecs
+   ps (for [_ (range n)] (shuffle (range k)))
+   a (->> ps
           (map m/permutation-matrix)
           (reduce m/add zero))
    b (->> (t/map m/permutation-matrix)
           (t/reduce m/add zero)
-          (t/tesser [vecs]))
+          (t/tesser [ps]))
    matrices-equal (if (= a b) "Yes" "No")]
   (println (str "Are a and b equal? " matrices-equal "\n"))
-  (m/pm a))
+  (m/pm a {:formatter (comp str int)} ))
 
 (System/exit 0)
