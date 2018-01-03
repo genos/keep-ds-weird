568bdeed6361500e88448a378c2737724c7f821f
genos <graham.enos@gmail.com>
2018-01-02 22:20:33 -0500

    code golf

--- parser_combinators/generate_data.py
+++ parser_combinators/generate_data.py
@@ -2,12 +2,12 @@
 # coding: utf-8
 """Generate fake weather data for parser combinbator examples"""
 
-import datetime
-import random
+from datetime import date, time, timedelta
+from random import choice, normalvariate, random, randrange, seed
 
 
 SENSOR_IDS = "ABC"
-START_DATE = datetime.date(2018, 1, 1)
+START_DATE = date(2018, 1, 1)
 WIND_DIRECTIONS = "NSEW"
 SPACES = [' ', '\t', ' \t', '\t ', '\t\t', '   ']
 
@@ -26,44 +26,44 @@ class Datum:
     ]
 
     def __init__(self, date):
-        self.sensor_id = random.choice(SENSOR_IDS)
+        self.sensor_id = choice(SENSOR_IDS)
         self.date = date
         self.time = None
-        if random.choice([True, False]):
-            self.time = datetime.time(
-                hour=random.randrange(0, 24),
-                minute=random.randrange(0, 60),
-                second=random.randrange(0, 60))
-        self.temperature = random.normalvariate(75, 10)
-        self.humidity = random.random()
+        if choice([True, False]):
+            self.time = time(
+                hour=randrange(0, 24),
+                minute=randrange(0, 60),
+                second=randrange(0, 60))
+        self.temperature = normalvariate(75, 10)
+        self.humidity = random()
         self.wind_direction = None
         self.wind_speed = None
-        if random.choice([True, False]):
-            self.wind_direction = random.choice(WIND_DIRECTIONS)
-            self.wind_speed = random.randrange(15)
+        if choice([True, False]):
+            self.wind_direction = choice(WIND_DIRECTIONS)
+            self.wind_speed = randrange(15)
 
     def __str__(self):
         return ''.join([
             self.sensor_id,
-            random.choice(SPACES),
+            choice(SPACES),
             self.date.isoformat(),
-            random.choice(SPACES),
+            choice(SPACES),
             '' if self.time is None else self.time.isoformat(),
-            random.choice(SPACES),
+            choice(SPACES),
             "{:2.1f}".format(self.temperature),
-            random.choice(SPACES),
+            choice(SPACES),
             "{:.0%}".format(self.humidity),
             '' if self.wind_direction is None or self.wind_speed is None
                else ''.join([
-                   random.choice(SPACES),
+                   choice(SPACES),
                    self.wind_direction,
-                   random.choice(SPACES),
+                   choice(SPACES),
                    "{:d}".format(self.wind_speed)
                ])
         ])
 
 
 if __name__ == '__main__':
-    random.seed(1729)
+    seed(1729)
     for i in range(10):
-        print(Datum(date=START_DATE + datetime.timedelta(i)))
+        print(Datum(date=START_DATE + timedelta(i)))
