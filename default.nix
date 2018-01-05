a02e795cc148110258aeebc5923a13dcc36eaf3d
genos <graham@quantifind.com>
2018-01-04 20:43:53 -0500

    A single `default.nix` ∀ the things

---
+++ default.nix
@@ -0,0 +1,14 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation rec {
+  name = "run_all_the_things";
+  shellHook = ''
+    it () {
+      for f in $(find . -mindepth 2 -name default.nix -print | sort); do
+        echo $f
+        cd $(dirname $f)
+        nix-shell --run it
+        cd - > /dev/null
+      done
+    }
+  '';
+}
