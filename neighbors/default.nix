6f60e8f825af5037d85f8230c2638c2b2b06e892
genos <graham@quantifind.com>
2019-01-23 11:55:00 -0500

    Pin specific nixpkgs version
    This defines `pkgs` to be `18.09` in `helpers.nix`, then pulls it in
    everywhere.

--- neighbors/default.nix
+++ neighbors/default.nix
@@ -1,10 +1,12 @@
-with import <nixpkgs> {};
-pkgs.stdenv.mkDerivation rec {
-  name = "nearest_neighbors.py";
-  buildInputs = [ python37 python37Packages.numpy ];
-  shellHook = ''
-    it () {
-      ${python37}/bin/python3 ${name}
-    }
-  '';
-}
+let
+  inherit (import ../helpers.nix) pkgs;
+in
+  pkgs.stdenv.mkDerivation rec {
+    name = "nearest_neighbors.py";
+    buildInputs = [ pkgs.python37 pkgs.python37Packages.numpy ];
+    shellHook = ''
+      it () {
+        ${pkgs.python37}/bin/python3 ${name}
+      }
+    '';
+  }
